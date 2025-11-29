371f0d99d3f28703f48822cbfe17cc0330cb1f86
YaelDev <jorge.yael333@gmail.com>
2025-11-29 08:44:51 -0600

    Manejando paralelismo (thread-safe) en el patron singleton

--- src/App.java
+++ src/App.java
@@ -1,16 +1,29 @@
-import creacionales.singleton.Logger;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 
+import creacionales.singleton.Logger;
 
 
 
 public class App {
 
     public static void main(String[] args) throws Exception {
-        Logger log = Logger.Instance();
-        log.WriteMessage("Hello World!");
+        App.singleton();
+    }
 
-        Logger log2 = Logger.Instance();
-        log2.WriteMessage("New logger?");
+
+    private static void singleton(){
+        // Probando si funciona en paralelismo
+        ExecutorService executor = Executors.newFixedThreadPool(20);
+
+        for (int i = 0; i < 100; i++) {
+            executor.submit(() -> {
+                Logger logger = Logger.Instance();
+                System.out.println(logger);
+            });
+        }
+
+        executor.shutdown();
     }
 
 }
--- src/creacionales/singleton/Logger.java
+++ src/creacionales/singleton/Logger.java
@@ -3,22 +3,31 @@ package creacionales.singleton;
 
 public class Logger {
 
-    private static Logger logger = null;
-
+    // Carga temprana
+    // private static Logger logger = new Logger();
 
     private Logger(){
     }
 
+    // en java las clases internas estaticas no se cargan hasta que son referenciadas
+    private static class Holder { // LAZY LOADING
+        private static final Logger INSTANCE = new Logger();
+    }
+
+    //! NO FUNCIONA CUANDO HAY PARALELISMO
+    // public static Logger Instance(){
+    //     if(logger == null){
+    //         System.out.println("Creando instancia del logger");
+    //         logger = new Logger();
+    //         return logger;
+    //     }
+
+    //     System.out.println("Retornando logger instanciado");
+    //     return logger;
+    // }
 
     public static Logger Instance(){
-        if(logger == null){
-            System.out.println("Creando instancia del logger");
-            logger = new Logger();
-            return logger;
-        }
-
-        System.out.println("Retornando logger instanciado");
-        return logger;
+        return Holder.INSTANCE;
     }
 
     public Boolean WriteMessage(String message){
